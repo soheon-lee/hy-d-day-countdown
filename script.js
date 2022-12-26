d9916afd31f944b3a0e4bf4b1edff05fe205736c
Soheon Lee <42701133+soheon-lee@users.noreply.github.com>
2022-12-26 22:07:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "26 Jan 2023";
+const newYears = "30 Dec 2022";
 
 function countdown() {
 	const newYearDate = new Date(newYears);
